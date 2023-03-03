77ba1c17330ac8beb77b26e516fe28797696d25f
Andre Rodrigues <andrepasr16@gmail.com>
2023-03-03 09:50:35 +0000

    fix cors

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000, http://www.example.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(cors({
     origin: ['http://localhost:3000', 'https://react-typescript-football-simulator-f0obufrmz.vercel.app'],
     credentials: true
