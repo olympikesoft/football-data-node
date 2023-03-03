8b9e5a4ee8a0eba0d2635f56ff9ae2dc7f5c0bca
Andre Rodrigues <andrepasr16@gmail.com>
2023-03-03 12:34:54 +0000

    changes

--- middleware/checkManager.js
+++ middleware/checkManager.js
@@ -3,6 +3,7 @@ var ManagerService = new ManagerService();
 
 const verifyHasManager = async (req, res, next) => {
   let user_id = req.user.id;
+  console.log(user_id, 'user_id');
   let control = await ManagerService.checkManagerbyUser(user_id);
   if (!control) {
     return res
