844e25cdd15dd97e48af766bf71775bc65609929
Andre Rodrigues <andrepasr16@gmail.com>
2023-03-23 10:56:57 +0000

    changes

--- knex.js
+++ knex.js
@@ -4,7 +4,7 @@ let knex = Knex({
   client: 'mysql2',
   debug: true,
   connection: {
-    'host': process.env.MYSQL_DATABASE,
+    'host': process.env.MYSQL_HOST,
     'user': process.env.MYSQL_USER,
     'database': process.env.MYSQL_DATABASE,
     'password': process.env.MYSQL_PASSWORD,
