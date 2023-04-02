2941b934fca690f46b53770395946c4624aa3da9
Paulo Rodrigues <paulorodrigues@Paulos-MBP.Home>
2023-04-02 22:21:43 +0100

    fix team

--- routes/team.js
+++ routes/team.js
@@ -11,9 +11,9 @@ const team = new TeamController();
 
 
 router.get('/manager/get/team', userMiddleware, verifyHasManager, (req, res, next) => team.getTeam(req, res, next));
-router.get('/get/list', userMiddleware, verifyHasManager, (req, res, next) => team.getTeams(req, res, next));
-router.post('/manager/create/team', upload.single("image"), userMiddleware, verifyHasManager, (req, res, next) =>  team.createTeam(req, res, next));
-router.post('/manager/team/linkLeague', userMiddleware, verifyHasManager, (req, res, next) =>  team.linkTeamLeague(req, res, next));
+router.get('/get/list', userMiddleware, (req, res, next) => team.getTeams(req, res, next));
+router.post('/manager/create/team', upload.single("image"), userMiddleware, (req, res, next) =>  team.createTeam(req, res, next));
+router.post('/manager/team/linkLeague', userMiddleware, (req, res, next) =>  team.linkTeamLeague(req, res, next));
 module.exports = router;
 
         
