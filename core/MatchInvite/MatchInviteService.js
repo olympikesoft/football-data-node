72b7a6080f2729cb38b11db5fe6d1654f3110af0
Paulo Rodrigues <paulorodrigues@Paulos-MBP.Home>
2023-04-03 12:30:12 +0100

    fix

--- core/MatchInvite/MatchInviteService.js
+++ core/MatchInvite/MatchInviteService.js
@@ -61,9 +61,8 @@ class MatchInviteService {
     "team2.id as team_home_id", 
     "match_invite.created_at",
     "match_invite.status",
-    "match_invite.id as match_invite_id"
   ])
-    .where({ 'match_invite_id': id })
+  .where("match_invite_id.id", id)
     .leftJoin("team", "team.id", "match_invite.user_one_id")
     .leftJoin('team as team2', 'team2.id', 'match_invite.user_two_id')
     .orderBy('match_invite.created_at');
