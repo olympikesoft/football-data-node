24ef5e62612d92677319055f79b338a102d58ae1
Paulo Rodrigues <paulorodrigues@Paulos-MBP.Home>
2023-04-07 10:23:56 +0100

    adding league stand

--- routes/league.js
+++ routes/league.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var LeagueController  = require('../core/League/LeagueController')
+var userMiddleware = require("../middleware/auth");
 
 const league = new LeagueController();
 
 router.get('/list', (req, res, next) => league.getLeagues(req, res, next));
+router.get('/team/stands', userMiddleware, (req, res, next) => league.getTeamLeagueStand(req, res, next));
+router.get('/team/stand', userMiddleware, (req, res, next) => league.getTeamLeaguePositionStand(req, res, next));
+
 router.get('/stands', (req, res, next) => league.getLeagueStand(req, res, next));
 
 module.exports = router;
--- schedule/league.js
+++ schedule/league.js
@@ -1,47 +1,72 @@
 const Cron = require("node-cron");
-var knex = require("../knex");
-var functions = require("../utils/functions");
+const knex = require("../knex");
+const functions = require("../utils/functions");
 
 module.exports = () => {
-  Cron.schedule("* * * * *", async () => {
-    const leagues = await knex("league").select("*");
+  Cron.schedule("0 * * * *", async () => {
+    const leagues = await knex("league")
+      .select("*")
+      .where("active", 1)
+      .andWhereRaw("teams_reached = teams_limit");
+
+      console.log(leagues);
+
     for (const league of leagues) {
-      if (league.teams_reached === league.teams_limit && league.active === 1) {
-        const teams = await knex("team_has_league")
-          .where("league_id", league.id)
-          .select("*");
+      console.log(league.id);
 
-        const roundMatches = functions.generateSchedule(
-          teams.map((el) => el.team_id)
-        );
+      const teams = await knex("team_has_league")
+        .where("league_id", league.id)
+        .select("*");
 
-        roundMatches.forEach((round) => {
-          round.forEach(async (match) => {
-            try {
-              const result = await knex("matchs").insert({
-                round: match.round,
-                team_home_id: match.home_team,
-                team_away_id: match.away_team,
-                date_game: match.date,
-                league_id: league.id,
-              });
-              console.log("Match created:", match);
-            } catch (error) {
-              console.log("Error creating match:", error);
-            }
+      const roundMatches = functions.generateSchedule(
+        teams.map((el) => el.team_id)
+      );
+
+      const matchesToInsert = [];
+
+      for (const round of roundMatches) {
+        for (const match of round) {
+          const matchDate = new Date(match.date);
+          const dateOnly = matchDate.toISOString().split("T")[0]; // format: 'YYYY-MM-DD'
+          matchesToInsert.push({
+            round: match.round,
+            team_home_id: match.home_team,
+            team_away_id: match.away_team,
+            date_game: dateOnly,
+            league_id: league.id,
           });
-        });
+        }
+      }
 
-        const round1Date = roundMatches[0][0].date;
-        const lastMatch = roundMatches[roundMatches.length - 1];
-        const lastDate = lastMatch.date;
-
-        // Update the league's rounds_completed and teams_reached fields
-        await knex("league").where("id", league.id).update({
-          active: 2,
-          date_start: round1Date,
-          date_end: lastDate,
+      try {
+        await knex.transaction(async (trx) => {
+          const batchSize = 1000;
+          for (let i = 0; i < matchesToInsert.length; i += batchSize) {
+            const batch = matchesToInsert.slice(i, i + batchSize);
+            await trx("matchs").insert(batch);
+          }
         });
+        console.log("Matches created for league:", league.id);
+      } catch (error) {
+        console.log("Error creating matches for league:", league.id, error);
+      }
+
+      const round1Date = roundMatches[0][0].date;
+      const lastMatch = roundMatches[roundMatches.length - 1];
+      const lastDate = lastMatch.date;
+
+      // Update the league's rounds_completed and teams_reached fields
+      try {
+        await knex("league")
+          .where("id", league.id)
+          .update({
+            active: 2,
+            date_start: new Date(round1Date),
+            date_end: new Date(lastDate),
+          });
+        console.log("League updated:", league.id);
+      } catch (error) {
+        console.log("Error updating league:", league.id, error);
       }
     }
   });
